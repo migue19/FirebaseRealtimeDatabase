e37d7622ad93f364a27a7591b6d564a0b0124652
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2021-02-01 16:44:13 -0600

    Initial Commit

---
+++ FirebaseRealtimeDatabase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseRealtimeDatabase
+//
+//  Created by Miguel Mexicano Herrera on 01/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
