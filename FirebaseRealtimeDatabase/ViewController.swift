3a8ec444789456d152f6fa91aba00dd5a3c56e18
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2021-02-01 19:14:04 -0600

    primer commit

--- FirebaseRealtimeDatabase/ViewController.swift
+++ FirebaseRealtimeDatabase/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
-
+import Firebase
 class ViewController: UIViewController {
-
+    @IBOutlet weak var conditionLabel: UILabel!
+    //let rootRef = Database.database().reference()
+    let conditionRef = Database.database().reference().child("condition")
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        conditionRef.observe(.value) { [self] (snapshot) in
+            conditionLabel.text = snapshot.value.debugDescription
+        }
+    }
+    @IBAction func foggyDidTouch(_ sender: Any) {
+        conditionRef.setValue("Foggy")
+    }
+    @IBAction func sunnyDidTouch(_ sender: Any) {
+        conditionRef.setValue("Sunny")
+    }
 }
 
